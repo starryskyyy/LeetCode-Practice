876f5a180dad85d1fcca066b973ebc9c7506ae66
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2022-09-16 16:29:15 -0400

    Create 1342. Number of Steps to Reduce a Number to Zero.java

---
+++ easy/1342. Number of Steps to Reduce a Number to Zero.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+class Solution {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while(num > 0){
+            if(num%2 != 0){
+                num = num - 1;
+                count++;
+            }
+            else{
+                num = num/2;   
+                count++;    
+            }
+        }
+        return count;
+    }
+}
